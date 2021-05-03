5d24fb5c584ff937aa34c8f4d1895acb3c5a06c6
Muqeet Ullah <muqeetullah07@gmail.com>
2021-05-03 20:48:25 +0500

    my movies app project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MqtMoviesApp from './mqtmoviesproject/components/MqtMoviesApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MqtMoviesApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
